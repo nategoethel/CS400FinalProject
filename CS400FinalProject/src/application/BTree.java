b4868eb6a6a6564d33a7efe2671c991cb63e21d7
nategoethel <44580393+nategoethel@users.noreply.github.com>
2019-11-25 21:45:29 -0600

    Added interface and basic methods for the tree.

---
+++ CS400FinalProject/src/application/BTree.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.List;
+
+/**
+ * Represents a B-Tree
+ * 
+ * @author Nate
+ *
+ * @param <T>
+ */
+public class BTree<T> implements BTreeADT<T> {
+
+  @Override
+  public void addNode(T node) throws IllegalArgumentException {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void removeNode(T node) throws IllegalArgumentException {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public T search(T node) throws IllegalArgumentException {
+    return node;
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public List<T> getAllNodes() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
