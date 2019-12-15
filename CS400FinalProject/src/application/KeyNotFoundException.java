e7bd7cdcb7f0199ae9a2bf03ee295c43c6642e5a
nategoethel <44580393+nategoethel@users.noreply.github.com>
2019-12-15 11:18:40 -0600

    added exception class

---
+++ CS400FinalProject/src/application/KeyNotFoundException.java
@@ -0,0 +1,11 @@
+package application;
+
+public class KeyNotFoundException extends Exception {
+  public KeyNotFoundException() {
+    super();
+  }
+  
+  public KeyNotFoundException(String message) {
+    super(message);
+  }
+}
